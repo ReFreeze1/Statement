16b7a3d90e15ba12a85d56a3294f3dab092997cc
Шматков Иван <shin911611@gmail.com>
2021-06-06 11:46:33 +0200

    Ready.

--- Statement.cpp
+++ Statement.cpp
@@ -1,7 +1,32 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
-    
+    std::ofstream file("statement.txt", std::ios::app);
+    std::string buffer;
+    std::string answer;
+    int payment;
+    do {
+        std::cout << "Input Name: ";
+        std::getline(std::cin, buffer);
+        file << buffer;
+
+        std::cout << "Input date (dd.mm.yyyy): ";
+        std::getline(std::cin, buffer);
+        file <<" "<< buffer;
+
+        std::cout << "Input payment: ";
+        std::cin >> payment;
+        file <<" "<< payment << std::endl;
+
+        std::cout << "Do you want add new record? (yes/no)";
+        std::cin >> answer;
+
+        std::cin.ignore(32767, '\n');
+
+    } while (answer != "no");
+    file.close();
 }
 
